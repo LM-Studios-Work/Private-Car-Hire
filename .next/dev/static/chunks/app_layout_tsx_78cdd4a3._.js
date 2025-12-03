cbfc0fa3ccc0971054ba8bdd126f6717bcc48551
Lupry Munyai <luprymunyai@gmail.com>
2025-12-03 20:05:37 +0200

    favicon

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++ .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/node_modules_6bb2ee03._.js"
+  "static/chunks/_82c2aac0._.js"
 ],
     source: "dynamic"
 });
